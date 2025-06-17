6b27b01690e25d0d9f6c13d3ea5debc2a12d550d
serendipityerr <panmk@shanghaitech.edu.cn>
2025-06-17 11:22:49 +0800

    google

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import os
 
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-print(author)
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
